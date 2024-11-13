8e9e51cb35f82e775c672c8e7b018a4c561637e1
GumayuLong <tothanhlong2002@gmail.com>
2024-11-13 16:30:35 +0700

    add feature search title (freetext search) and job_type(combobox)

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,11 +1,27 @@
 class JobsController < ApplicationController
   # Constant
-  ELEMENT_EACH_PAGE = 10
+  ELEMENT_EACH_PAGE = 5
 
   def index
-    # @job_types = Job.job_type.keys
+    # Lấy tất cả jobs trong table job
     @jobs = Job.all
-    @jobs = Job.paginate(page: params[:page], per_page: ELEMENT_EACH_PAGE)
+    # Lấy tất cả các job_type duy nhất từ bảng jobs
+    @job_types = Job.distinct.pluck(:job_type)
+
+    # Tìm kiếm theo job_types (nếu có)
+    if params[:job_type].present? && params[:job_type] != 'All' 
+      if !params[:title].present?
+        @jobs = @jobs.where(job_type: "%#{params[:job_type]}").page(params[:page]).per_page(ELEMENT_EACH_PAGE)
+      else
+        @jobs = @jobs.where('jobs.title LIKE ? AND jobs.job_type LIKE ?', "%#{params[:title]}%", "%#{params[:job_type]}%").page(params[:page]).per_page(ELEMENT_EACH_PAGE)
+      end
+    else
+      if params[:title].present?
+        @jobs = @jobs.where('jobs.title LIKE ?', "%#{params[:title]}%").page(params[:page]).per_page(ELEMENT_EACH_PAGE)
+      else
+        @jobs = Job.paginate(page: params[:page], per_page: ELEMENT_EACH_PAGE)
+      end
+    end
   end
 
   def show
