93a905b07f5bb5150c28088a7e6e1b4af3bcfd6d
GumayuLong <tothanhlong2002@gmail.com>
2024-11-13 11:21:33 +0700

    CI check

--- test/controllers/jobs_controller_test.rb
+++ test/controllers/jobs_controller_test.rb
@@ -1,12 +1,12 @@
-require "test_helper"
+require 'test_helper'
 
 class JobsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  test 'should get index' do
     get jobs_index_url
     assert_response :success
   end
 
-  test "should get show" do
+  test 'should get show' do
     get jobs_show_url
     assert_response :success
   end
