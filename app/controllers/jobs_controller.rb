696be1e92bbb2318e5bd8a0f33578d276741e323
GumayuLong <tothanhlong2002@gmail.com>
2024-11-14 17:10:14 +0700

    update feature search and delete unnecessary files

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -3,25 +3,13 @@ class JobsController < ApplicationController
   ELEMENT_EACH_PAGE = 5
 
   def index
-    # Lấy tất cả jobs trong table job
+    # Lọc theo job_type nếu có, và title nếu có
     @jobs = Job.all
-    # Lấy tất cả các job_type duy nhất từ bảng jobs
-    @job_types = Job.distinct.pluck(:job_type)
+    @jobs = @jobs.where(job_type: params[:job_type]) if params[:job_type].present? && Job.job_types.keys.include?(params[:job_type])
+    @jobs = @jobs.where('jobs.title LIKE ?', "%#{params[:title]}%") if params[:title].present?
 
-    # Tìm kiếm theo job_types (nếu có)
-    if params[:job_type].present? && Job.job_types.keys.include?(params[:job_type])
-      if !params[:title].present?
-        @jobs = @jobs.where(job_type: Job.job_types[params[:job_type]]).page(params[:page]).per_page(ELEMENT_EACH_PAGE)
-      else
-        @jobs = @jobs.where('jobs.title LIKE ? AND jobs.job_type = ?', "%#{params[:title]}%", Job.job_types[params[:job_type]]).page(params[:page]).per_page(ELEMENT_EACH_PAGE)
-      end
-    else
-      if params[:title].present?
-        @jobs = @jobs.where('jobs.title LIKE ?', "%#{params[:title]}%").page(params[:page]).per_page(ELEMENT_EACH_PAGE)
-      else
-        @jobs = Job.paginate(page: params[:page], per_page: ELEMENT_EACH_PAGE)
-      end
-    end
+    # Paginate kết quả
+    @jobs = @jobs.page(params[:page]).per_page(ELEMENT_EACH_PAGE)
   end
 
   def show
